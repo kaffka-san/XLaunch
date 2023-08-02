63d69f44f20b1adab5a537c98d028866c357a052
Kaffka <127501232+kaffka-san@users.noreply.github.com>
2023-08-02 19:02:00 +0200

    Revert "Feature/persitence"

---
+++ XLaunch/LaunchesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  XLaunch
+//
+//  Created by Anastasia Lenina on 31.07.2023.
+//
+
+import UIKit
+
+class LaunchesViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemPink
+  }
+}
